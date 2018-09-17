55ca2e46690c9a37003d362fdabad5352fd28bc5
Jason Olson <jolson88@outlook.com>
2018-09-17 16:57:05 -0700

    Adds basic color interpolation

--- src/interpolation.ts
+++ src/interpolation.ts
@@ -1,4 +1,4 @@
-import { Function } from './types';
+import { Function, IColor, makeColor } from './types';
 
 /**
  * Compresses a value within a given range to the percentage range of 0..1. Values outside
@@ -15,5 +15,27 @@ export function toPercentage(rangeMin: number, rangeMax: number): Function<numbe
         }
         const diff = val - rangeMin;
         return diff / totalRange;
+    };
+}
+
+/**
+ * Calculates an interpolated color between the start color and end color
+ * @param startColor The first color to be interpolated between
+ * @param endColor The second color to be interpolated between
+ * @returns Function that accepts an interpolation percentage between 0..1 and returns a color
+ */
+export function lerpColor(startColor: IColor, endColor: IColor): Function<number, IColor> {
+    const diffR = endColor.r - startColor.r;
+    const diffG = endColor.g - startColor.g;
+    const diffB = endColor.b - startColor.b;
+    const diffA = endColor.a - startColor.a;
+
+    return (lerp) => {
+        return makeColor(
+            startColor.r + (diffR * lerp),
+            startColor.g + (diffG * lerp),
+            startColor.b + (diffB * lerp),
+            startColor.a + (diffA * lerp),
+        );
     };
 }
